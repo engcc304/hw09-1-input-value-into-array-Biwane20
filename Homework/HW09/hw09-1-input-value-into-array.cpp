7632fbc6c1ef0e2327d60261208ab6e783cd430b
Biwane20 <biwbew20@gmail.com>
2023-10-05 13:33:13 +0700

    9-1

--- Homework/HW09/hw09-1-input-value-into-array.cpp
+++ Homework/HW09/hw09-1-input-value-into-array.cpp
@@ -1,27 +1,62 @@
 /*
     จงเขียนโปรแกรมภาษาซีเพื่อเก็บข้อมูลเข้าไปในอาเรย์จำนวน N ค่า และแสดงผลลัพธ์ออกมาบนหน้าจอ (โดยหากกรอก -1 ให้หยุดกรอกค่าและแสดงผลทันที)
-    
+
     Test case:
-        Input [0] : 
+        Input [0] :
             1
-        Input [1] : 
+        Input [1] :
             1
-        Input [2] : 
+        Input [2] :
             3
-        Input [3] : 
+        Input [3] :
             9
-        Input [4] : 
+        Input [4] :
             8
-        Input [5] : 
+        Input [5] :
             2
-        Input [6] : 
+        Input [6] :
             7
-        Input [7] : 
+        Input [7] :
             8
-        Input [8] : 
+        Input [8] :
             -1
     Output:
         Index = 0 1 2 3 4 5 6 7
         Array = 1 1 3 9 8 2 7 8
 
 */
+#include <stdio.h>
+int main()
+{
+
+    int num[100];
+    int n = 100;
+    int c = 0;
+    // printf( "N = " ) ;
+    // scanf( "%d",&n ) ;
+    for (int i = 0; i <= n; i++)
+    {
+        printf("Input [%d] :\n", i);
+        scanf("%d", &num[i]);
+        c++;
+        if (num[i] < 0)
+        {
+            break;
+        }
+    }
+
+    printf("Index =");
+    for (int i = 0; i < c - 1; i++)
+    {
+        printf(" %d", i);
+    }
+
+    printf("\nArray = ");
+
+    for (int i = 0; i < c - 1; i++)
+    {
+        printf("%d ", num[i]);
+    }
+
+    return 0;
+}
